f32ab324ddcf562ead5f62a9d3eb56908316c5a6
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-27 14:35:13 -0400

    mvp commit for if statement.js

---  ifstatements.js
+++  ifstatements.js
@@ -54,3 +54,54 @@ if (pCode = 10010) {
     console.log("pCode is incorrect")
 }
 
+// Code an if statement. Test whether a variable has a particular numerical value. If so, assign a new value to that variable, as in x = 1;
+
+x = 1;
+
+if (x === 1) {
+    x = 2;
+    }
+
+// Time goal: 15 seconds. (The timer starts when you click in the answer field.)
+// What are the 4 bracket characters in every if statement, in order?.
+
+ if (x = i){
+
+ }
+
+//  Time goal: 35 seconds. Code the first line of an if statement that tests whether one variable has the same value as another.
+
+ if (name === nicename){
+
+ }
+
+//  Time goal: 35 seconds. Code the first line of an if statement that tests whether a variable has the value of a particular string.
+
+ if (hi === "hello") {
+
+ }
+
+ // Complete the statement. If the condition is true, display a box that asks the user a question. Assign the answer to a variable that hasn't been declared beforehand.
+
+ if (x === y) {
+    var x = prompt ("question"); 
+ }
+
+// Code an if statement that tests whether a number equals itself.
+// If the condition is true (it will be), display a congratulations alert.
+// Click the Run It button to run your code live.
+
+if (x === 10) {
+    alert("Congrultions");
+}
+
+// Code a prompt asking for your first name.
+// Code an if statement that tests whether the name you entered equals your name.
+// If the condition is true (it will be), display an alert that greets you by name.
+// Click the Run It button to run your code live.
+
+var name = prompt ("what is your name ");
+name = "justin";
+if ("name" === "name") {
+    alert("Good Evening " + name);
+}
