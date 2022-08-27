fa0336c0bfbf431c7434d3e48383e2065c75d126
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-27 00:04:49 -0400

    added awnsers to if statments

---  ifstatements.js
+++  ifstatements.js
@@ -22,3 +22,35 @@ if (city = "Las Vegas") {}
     // In the above statement, what character is incorrect?
 
     // ===
+
+var age = 20;
+if (age < 18) {
+    console.log("underage");
+}
+
+var age = 21
+if (age < 21)  {
+    console.log("old enought to drink");
+} else {
+    console.log("not drinking age");
+}
+var firstName = John;
+
+// Complete the if statement. If the condition is true
+
+if (firstName === "John") {
+    console.log("correct name given!");
+} else {
+    console.log("try another name please");
+}
+
+// Code an if statement that tests whether pCode is "10010". If so, assign to the variable city the string "New York". city has already been declared.
+
+var pCode = 10010;
+
+if (pCode = 10010) {
+    console.log("passcode is correct");
+} else {
+    console.log("pCode is incorrect")
+}
+
