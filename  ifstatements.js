fe52e201daf5f03b95efc2bae090cb969c291833
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-26 23:41:41 -0400

    intial commit for if statement

---
+++  ifstatements.js
@@ -0,0 +1,24 @@
+var age = 20;
+if (age < 18) {
+	console.log("underage");
+} else {
+	console.log("let em in!");
+}
+
+// What are the first 4 characters of an if statement?
+
+if (x = 1);
+
+// What character ends the first line of an if statement?
+
+// {
+
+// What is the last line of an if statement?
+
+// }
+
+if (city = "Las Vegas") {}
+
+    // In the above statement, what character is incorrect?
+
+    // ===
